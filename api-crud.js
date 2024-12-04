96d4243f5dd0509247aaf5ec206473c15853c513
adrijimdev <adrianjimenez1994@gmail.com>
2024-12-04 17:01:25 +0100

    api funcional conectada a mongodb

--- api-crud.js
+++ api-crud.js
@@ -1,6 +1,10 @@
+const mongoose = require('mongoose'); 
 const express = require('express'); //importación del módulo express.js
+require('dotenv').config();
 const app = express(); //crea una instancia de express donde se definirán las rutas
 app.use(express.json()); //middleware que interpreta el cuerpo de las solicitudes como json
+
+const MONGO_URI = process.env.MONGO_URI;
 
 //para poder ejecutar el servidor local con node.js
 const PORT = 3000;
@@ -8,58 +12,95 @@ app.listen(PORT, () => {
     console.log(`servidor activo en puerto ${PORT}`);
 });
 
-let idCount = 1;
-let noteList = [];
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Conexión correcta a MongoDB'))
+    .catch(err => console.error('Error al conectar a MongoDB:', err));
 
-class Note {
-    constructor(title, content) {
-        this.id = idCount;
-        idCount += 1;
-        this.title = title;
-        this.content = content;
-        this.createdAt = new Date();
+const noteSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+app.post('/notes', async (req, res) => {
+    try {
+        const { title, content } = req.body;
+        if (!title || !content) {
+            return res.status(400).json({ error: 'Tu nota debe tener título y contenido' });
+        }
+        
+        const newNote = new Note({ title, content });
+        const savedNote = await newNote.save();
+        res.status(201).json(savedNote);
     }
-}
-
-app.post('/notes', (req, res) => {
-    const { title, content } = req.body;
-
-    const newNote = new Note(title, content);
-    noteList.push(newNote);
-
-    res.status(201).json(newNote);
+    catch (error) {
+        res.status(500).json({ error: 'Error al crear la nota' });
+    }
 });
 
 //obtener todas las notas
-app.get('/notes', (req, res) => {
-    res.json(noteList);
+app.get('/notes', async (req, res) => {
+    try {
+        const notes = await Note.find();
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener las notas' });
+    }
 });
 
 //obtener nota concreta
-app.get('/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const note = noteList.find(n => n.id === parseInt(id));
+app.get('/notes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const note = await Note.findById(id);
 
-    res.json(note); 
+        if (!note) {
+            return res.status(404).json({ error: "No se encuentra la nota" })
+        }
+
+        res.json(note); 
+    } catch {
+        return res.status(500).json({ error: "Error obteniendo la nota"} )
+    }
 });
 
-app.put('/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const note = noteList.find(n => n.id === parseInt(id));
-    const { title, content } = req.body;
+//modificar nota
+app.put('/notes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content } = req.body;
+        if (!title || !content) {
+            return res.status(400).json({ error: 'Tu nota debe tener título y contenido' });
+        }
+        
+        const updatedNote = await Note.findByIdAndUpdate(
+            id,
+            { title, content, updatedAt: new Date() },
+            { new:true } //esto devuelve la nota actualizada
+        );
 
-    note.title = title;
-    note.content = content;
-    noteList.push(newNote);
-
-    res.json(note);
+        if(!updatedNote) {
+            return res.status(404).json({ error: "No se encuentra la nota" })
+        }
+        
+        res.json(updatedNote);
+    } catch {
+        return res.status(500).json({ error: "Error actualizando la nota"} )
+    }
 });
 
-app.delete('/notes/:id', (req, res) => {
-    const { id } = req.params;
-    const noteIndex = noteList.findIndex(n => n.id === parseInt(id));
-
-    noteList.splice(noteIndex, 1);
-
-    res.status(204).send();
+//eliminar nota
+app.delete('/notes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedNote = await Note.findByIdAndDelete(id);
+        if (!deletedNote) {
+            return res.status(404).json({ error: 'No se encuentra la nota' });
+        }
+        res.status(204).send(); // Respuesta sin contenido
+    } catch (error) {
+        res.status(500).json({ error: 'Error eliminando la nota' });
+    }
 });
