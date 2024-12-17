ad9a854808bcc77f013949fccd2c2868283b3294
adrijimdev <adrianjimenez1994@gmail.com>
2024-12-17 11:53:09 +0100

    creado método login, ahora se encriptan las contraseñas al registrar un usuario

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 router.post('/', async (req, res) => {
     try {
         const { username, password } = req.body;
+
         if (!username || !password) {
             return res.status(400).json({ error: 'Tu usuario debe tener nombre y contraseña' });
         }
-        
-        const newUser = new User({ username, password });
+
+        //
+        const hashedPassword = await argon2.hash(password);
+
+        const newUser = new User({ username, password: hashedPassword });
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     }
     catch (error) {
         res.status(500).json({ error: 'Error al crear el usuario' });
+    }
+});
+
+//método login
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        if (!username || !password) {
+            return res.status(400).json({ error: 'El nombre de usuario y la contraseña son obligatorios' });
+        }
+
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+        }
+
+        //se utiliza argon2 para verificar que los datos del usuario sean correctos
+        const isPasswordValid = await argon2.verify(user.password, password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Usuario o contraseña incorrectos' });
+        }
+
+        //se genera el token que se utilizará en el frontend para verificar el login, utilizando una clave secreta creada por mí
+        const token = jwt.sign(
+            { userId: user._id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' } //configuramos el token para expirar en una hora
+        );
+
+        res.status(200).json({
+            message: 'Login correcto',
+            token: token,
+            userId: user._id
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error al iniciar sesión' });
     }
 });
 
